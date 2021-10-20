a08b3ae9c1cda404dea82a75ea1da646d8b27fc0
Nikola Jovanović <nidza.jov@gmail.com>
2021-10-20 21:33:51 +0200

    start of new app called tasks-app

---
+++ VueVezbe/tasks-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    Hello, world!
+    <custom-button/>
+    <custom-button/>
+    <custom-button/>
+  </div>
+</template>
+
+<script>
+import CustomButton from './components/CustomButton.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CustomButton,
+  },
+};
+</script>
+
+<style lang="less">
+
+</style>
