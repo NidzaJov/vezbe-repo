8e39be3db40ecbe6e244ab995b25bd25b3c99db1
Nikola Jovanović <nidza.jov@gmail.com>
2021-10-23 20:34:27 +0200

    Add a Vuex store

--- VueVezbe/tasks-app/src/main.js
+++ VueVezbe/tasks-app/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
+import store from './store';
 
 Vue.config.productionTip = false;
 
 const app = new Vue({
+  store,
   render: (h) => h(App),
 });
 app.$mount('#app');
