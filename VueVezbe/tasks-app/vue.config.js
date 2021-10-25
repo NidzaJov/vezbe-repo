01d91b3c79617f52dfb54aa60e9cab5c41633342
Nikola Jovanović <nidza.jov@gmail.com>
2021-10-25 22:08:57 +0200

    another tweak and build

--- VueVezbe/tasks-app/vue.config.js
+++ VueVezbe/tasks-app/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://nidzajov.github.io//vezbe-repo/VueVezbe/tasks-app/dist/'
+    ? 'https://nidzajov.github.io/vezbe-repo/VueVezbe/tasks-app/dist/'
     : '/',
 };
