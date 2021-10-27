4a44a9becaaebdeee678afdeefa84a928adb2878
Nikola Jovanović <nidza.jov@gmail.com>
2021-10-27 23:01:01 +0200

    DB connection and creating Express app (server)

--- ReactVezbe/ReactVezba5/src/main.js
+++ ReactVezbe/ReactVezba5/src/main.js
@@ -1,1 +1,18 @@
-console.log('hello!')
+const database = require('./modules/database');
+const config = require('./config');
+
+(async () => {
+    console.info('Starting the app...');
+    console.info('Connecting to the DB...');
+    await database.connect();
+    console.info(("Connected to the DB."));
+
+    console.info('Creating the express app...');
+    const express = require('express');
+    const app = express();
+
+    console.info('Starting the app...');
+    app.listen(config.app.port, () => {
+        console.info(`Server is listening on port ${config.app.port}...`)
+    })
+})();
---
+++ ReactVezbe/ReactVezba5/src/modules/database/index.js
@@ -0,0 +1,34 @@
+const config = require('../../config');
+const { MongoClient } = require('mongodb');
+const { usersCollection } = require('../../constants');
+
+const connectionString = `mongodb://${config.db.username}:${config.db.password}@localhost/${config.db.dbname}`;
+
+class Database {
+    async connect() {
+        const client = new MongoClient(connectionString, {
+            useUnifiedTopology: true,
+            authSource: config.db.dbname
+        });
+        this.connection = await client.connect();
+
+        await this.DB
+         .collection(usersCollection)
+         .createIndex({email: 1}, {unique: true})
+    }
+
+    async disconnect() {
+        this.connection.close();
+    }
+
+    get DB () {
+        try {
+            return this.connection.db(config.db.dbname);
+        }
+        catch(e) {
+            console.error('Database unavailable:', e)
+        }
+    }
+}
+
+module.exports = new Database();
