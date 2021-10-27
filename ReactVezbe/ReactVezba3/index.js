d4268bd3518d1ff616b4a10914ec81941cf1ba0f
Nikola Jovanović <nidza.jov@gmail.com>
2021-10-27 12:54:22 +0200

    Route params, mutiple methods in single route, Router, global middleware and Router middleware

--- ReactVezbe/ReactVezba3/index.js
+++ ReactVezbe/ReactVezba3/index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
 const port = 4000;
-
-app.get('/', function(req, res) {
-    console.log('Get is called');
-    res.end();
+const usersRouterInfo = require('./routes/users')
+/*
+app.get('/flights/:from-:to', function(req, res) {
+    const params = req.params;
+    console.log('Get is called for /about!', params);
+    res.status(200);
+    res.send('Hello there!')
 });
+*/
+/*
+app.route('/users')
+  .get(function(req, res) {
+      res.send('Hello GET!')
+  })
+  .post(function(req, res) {
+      res.send('Hello POST!')
+  })
+*/
+/*
+app.use('/users', function(req, res, next) {
+    console.log('My global middleware is executing!');
+    next();
+})
+*/
+
+app.get('/about', function(req, res) {
+    console.log('About page GET!');
+    res.send('It is a server!');
+})
+
+app.use(usersRouterInfo.path, usersRouterInfo.router)
 
 app.listen(port, function() {
     console.log(`Server listening on port ${port}`)
---
+++ ReactVezbe/ReactVezba3/routes/users.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const usersRouter = express.Router();
+
+const firstMiddleware = function(req, res, next) {
+    console.log('First /users middleware is executing!')
+    const userLoggedIn = true;
+    res.someData = 'TEST';
+    if (userLoggedIn) {
+        next();
+    }
+    else {
+        res.sendStatus(401);
+    }
+    console.log('First  /users middleware is done!');
+};
+
+const secondMiddleware = function(req, res, next) {
+    console.log('Second /users middleware is executing!');
+    console.log('Second midleware reads data: ', res.someData);
+    next();
+    console.log('Second /users middleware is done!')
+};
+
+usersRouter.use([firstMiddleware, secondMiddleware]);
+
+
+usersRouter.get('/:usersId', function(req, res) {
+    console.log('User ID GET!')
+    res.send('Hello Get!')
+});
+
+usersRouter.post('/', function(req, res) {
+    console.log('User ID POST!')
+    res.send('Hello POST!');
+})
+
+usersRouter.put('/:userId', function(req, res) {
+    console.log('User ID PUT!')
+    res.send("Hello PUT!");
+});
+
+module.exports = {
+    path: '/users',
+    router: usersRouter,
+}
