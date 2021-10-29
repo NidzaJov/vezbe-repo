d9776840f9e02a11b4337fe09d8710b63987475a
Nikola Jovanović <nidza.jov@gmail.com>
2021-10-29 18:21:46 +0200

    Update by PUT

--- ReactVezbe/ReactVezba5/src/modules/services/todos/index.js
+++ ReactVezbe/ReactVezba5/src/modules/services/todos/index.js
@@ -1,8 +1,7 @@
-const { OnjectID } = require('mongodb');
+const { ObjectID } = require('mongodb');
 const { validateKeysExists } = require("../../helpers");
 const { DB } = require('../../database');
 const { todosCollection } = require('../../../constants');
-const { ObjectID } = require('bson');
 
 const todoRequiredKeys = ['owner', 'title', 'items', 'sharedWith', 'lastModified'];
 const todoItemRequiredKeys = ['done', 'text']
@@ -12,8 +11,11 @@ class TodosService {
         return DB.collection(todosCollection);
     }
 
-    validateTodoObject(todo) {
-        validateKeysExists(todoRequiredKeys, todo);
+    validateTodoObject(todo, existing = false) {
+        validateKeysExists([
+            ...(existing ? ['_id'] : []),
+            ...todoRequiredKeys
+        ], todo);
         for (const item of todo.items) {
             validateKeysExists(todoItemRequiredKeys, item);
         }
@@ -38,14 +40,39 @@ class TodosService {
         
     }
 
-    async update() {}
+    async update(ownerId, todo) {
+        this.validateTodoObject(todo, true);
+        const { _id } = todo;
+        delete todo._id;
+        delete todo.owner;
+
+        return await TodosService.collection.updateOne({
+            _id: new ObjectID(_id),
+            owner: new ObjectID(ownerId)
+        }, {
+            $set: {...todo, lastModified: new Date() }
+        });
+    }
     async updateField() {}
     async share() {}
     async delete() {}
 
-    async findbyId() {}
-    async finfAllByUserId() {}
-    async findAllSharedWithUserId() {}
+    async findbyId(ownerId, todoId) {
+        return await TodosService.collection.findOne({
+            _id: new ObjectID(todoId),
+            owner: new ObjectID(ownerId),
+        });
+    }
+    async findAllByUserId(ownerId) {
+        return await TodosService.collection
+            .find({owner: new ObjectID(ownerId)})
+            .toArray()
+    }
+    async findAllSharedWithUserId(userId) {
+        return await TodosService.collection
+        .find({ sharedWith: { $in: [new ObjectId(userId)] }})
+        .toArray();
+    }
 }
 
 module.exports = new TodosService();
--- ReactVezbe/ReactVezba5/src/router/todos/index.js
+++ ReactVezbe/ReactVezba5/src/router/todos/index.js
@@ -1,20 +1,47 @@
 const express = require('express');
 const { Router } = express;
 const authMiddleware = require('../../middleware/auth');
+const todos = require('../../modules/services/todos');
 const todosService = require('../../modules/services/todos')
 
 const todosRouter = Router();
 
 todosRouter.use(authMiddleware);
 
+todosRouter.get('/:todoId?', async function(req, res) {
+    const { todoId } = req.params;
+    if (todoId) {
+        const todo = await todosService.findbyId(req.user._id, todoId);
+        res.json(todo);
+    }
+    else {
+        const todos = [
+            ...(await todosService.findAllByUserId(req.user._id)),
+            ...(await todosService.findAllSharedWithUserId(req.user._id))
+        ];
+        res.json(todos);
+    }
+})
+
 todosRouter.post('/', async function(req, res) {
-    const todo = req.body;
-    console.info('Create todo got:', todo);
     try{
+        const todo = req.body;
+        console.info('Create todo got: ', todo);
         await todosService.create(req.user._id, todo);
         res.sendStatus(201);
     } catch (e) {
         console.error('Todo not created', e);
+        res.sendStatus(500);
+    }
+})
+
+todosRouter.put('/', async function(req, res) {
+    try {
+        const todo = req.body;
+        await todosService.update(req.user._id, todo);
+        res.sendStatus(200);
+    } catch (e) {
+        console.error('Todo not updated', e);
         res.sendStatus(500);
     }
 })
