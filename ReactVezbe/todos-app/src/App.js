bd93d0e24ae195550bcc9e50fedc91b6cb571833
Nikola Jovanović <nidza.jov@gmail.com>
2021-11-10 00:16:24 +0100

    Looping over array and rendering it

--- ReactVezbe/todos-app/src/App.js
+++ ReactVezbe/todos-app/src/App.js
@@ -5,10 +5,25 @@ import { useCounter } from './hooks/useCounter'
 export function App() {
   
   const { counter, decrementCounter, incrementCounter } = useCounter(5);
+  const messages = [];
 
   const divisibleByTen = (counter % 10) === 0;
-  const message = divisibleByTen ? (<div>Number is divisible by 10.</div>): null
+  const divisibleByFifteen = (counter % 15) === 0;
+  const divisibleByTweenty = (counter % 20) === 0;
 
+  if (divisibleByTen) messages.push('Counter is divisible by 10.');
+  if (divisibleByFifteen) messages.push('Counter is divisible by 15');
+  if (divisibleByTweenty) messages.push('Counter is divisible by 20.');
+
+  const messageElements = messages
+  .map(
+    (msg, idx) => (
+    <div key={`message-${idx}`}>
+      {msg}
+      </div>
+    )
+  );
+  console.log(messageElements)
     return (
       <>
         <Counter counterValue={ counter }/>
@@ -18,7 +33,7 @@ export function App() {
         <CustomButton isClicked={decrementCounter}>
           Decrement
         </CustomButton>
-        {message}
+        {messageElements}
       </>
     )
 
