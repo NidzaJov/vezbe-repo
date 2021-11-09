abea7bff8983251ec50bcba28d4f11d8b7666a64
Nikola Jovanović <nidza.jov@gmail.com>
2021-11-09 21:40:46 +0100

    Modify App.js to export App as extension of class Component, immutability error in changing state

--- ReactVezbe/todos-app/src/App.js
+++ ReactVezbe/todos-app/src/App.js
@@ -1,10 +1,37 @@
+import { Component } from 'react';
 
+export class App extends Component {
+  constructor() {
+    super();
 
-export default function App() {
-  return (
-    <div>
-      Hello React!
-    </div>
-  );
+    this.state = {
+      person: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+      counter: 0,
+    };
+  }
+
+  buttonClickHandler() {
+    this.setState({
+      person: {
+        firstName: 'Jane'
+      }
+    });
+  }
+
+  render() {
+    console.log('Component rendering');
+
+    return (
+      <div>
+        Counter value: { this.state.counter }
+        <button onClick={ () => this.buttonClickHandler() }>Increment</button>
+        <div>{ `${this.state.person.firstName} ${this.state.person.lastName}` }</div>
+      </div>
+    )
+  }
+
 }
 
