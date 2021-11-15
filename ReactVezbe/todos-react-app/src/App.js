c76bbba02be817c8f28e735083633219dcfb4d6d
Nikola Jovanović <nidza.jov@gmail.com>
2021-11-15 14:52:43 +0100

    Routing for login and register views

--- ReactVezbe/todos-react-app/src/App.js
+++ ReactVezbe/todos-react-app/src/App.js
@@ -3,7 +3,7 @@ import TodoForm from './components/TodoForm';
 import { useSelector } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from './views/Login';
-import Register from './views/MainLayout';
+import Register from './views/Register';
 
 export default function App() {
   
